85cf9f7f262c4704bd1db8a76b35eb79508d8b80
Jayden Sim <39719057+bluebir6@users.noreply.github.com>
2020-08-13 17:58:39 +1000

    yes

---
+++ dist/precache-manifest.dd01a52962c224935ce35689848d9670.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb82d15500c8190d247",
+    "url": "/css/app.df9b022c.css"
+  },
+  {
+    "revision": "b939615b6688d6fb4e24",
+    "url": "/css/editor.d9a0ac7c.css"
+  },
+  {
+    "revision": "bec815502e9a3ce2480e36fd0df8221a",
+    "url": "/img/BluebirdCloud Canvas Icon - Dark.bec81550.svg"
+  },
+  {
+    "revision": "9a062792f59f7043e9b65ae9179beb45",
+    "url": "/img/BluebirdCloud Canvas Icon - Light.9a062792.svg"
+  },
+  {
+    "revision": "9a06434038b216c5b42780473007de3a",
+    "url": "/img/Eraser.9a064340.svg"
+  },
+  {
+    "revision": "f82a28c1db8c4d97e6264df1561ec322",
+    "url": "/img/Eraser.f82a28c1.svg"
+  },
+  {
+    "revision": "850ad4c8a622805434d95807f32c5b89",
+    "url": "/img/Pen.850ad4c8.svg"
+  },
+  {
+    "revision": "a44af2416a11d1f1f8f8ff41a4b40512",
+    "url": "/img/Pen.a44af241.svg"
+  },
+  {
+    "revision": "e99c1f281019f9cb15d8574444c924f0",
+    "url": "/img/Pen_BLUE.e99c1f28.svg"
+  },
+  {
+    "revision": "e281bf193a1229370438ef38b3c8b38d",
+    "url": "/img/Pen_GREEN.e281bf19.svg"
+  },
+  {
+    "revision": "d0600f3ff7fb486ca2a8b2e11cdfbfdd",
+    "url": "/img/Pen_PURPLE.d0600f3f.svg"
+  },
+  {
+    "revision": "6fcd2b0f9308b97c778b9f5135beeeb0",
+    "url": "/img/Pen_RED.6fcd2b0f.svg"
+  },
+  {
+    "revision": "141fdb29391135840f84aeecb7053d4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eb82d15500c8190d247",
+    "url": "/js/app.ce4b6a63.js"
+  },
+  {
+    "revision": "dab14f17247674140c29",
+    "url": "/js/chunk-vendors.673e8ca3.js"
+  },
+  {
+    "revision": "b939615b6688d6fb4e24",
+    "url": "/js/editor.5b1c3085.js"
+  },
+  {
+    "revision": "95af257998f113df13f8096801e43bea",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
